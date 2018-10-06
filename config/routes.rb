e07b8605c84106ac51e353edcb6d5625b7fd988e
captproton <carl@wdwhub.net>
2018-10-06 10:36:58 -0700

    INIT: layout for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :services, :orders, :line_items, :carts, :consultations
+
+  resources :accounts do
+    resources :notes, only: [:index, :new]
+    resources :service_agreements, shallow: true
+    # resources :consultations, only: [:index, :new]
+  end
   mount RailsAdmin::Engine => '/backstage', as: 'rails_admin'
 end
